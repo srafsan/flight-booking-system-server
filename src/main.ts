b9e6a28d1f4c97749552a215fbb474061406b60b
srafsan <srafsan98@tutanota.com>
2024-11-27 23:01:23 +0600

    configs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dapper-kringle-329e13.netlify.app',
     credentials: true,
   });
 
